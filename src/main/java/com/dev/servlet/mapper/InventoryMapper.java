m-feliciano/servlets

features: v1.4:
- REST controllers
- Second Layer of Security
- Reusable components: services, buttons, pagination, etc.
- Custom HTML template engine
- Cache expiration policy
- Unit Testing
- logback.xml, ehcache.xml, lombok

src/main/java/com/dev/servlet/mapper/InventoryMapper.java
@@ -1,41 +1,47 @@
 package com.dev.servlet.mapper;
 
-import com.dev.servlet.dto.InventoryDto;
+import com.dev.servlet.dto.InventoryDTO;
 import com.dev.servlet.pojo.Inventory;
 
 public final class InventoryMapper {
+
     private InventoryMapper() {
+        throw new IllegalStateException("InventoryMapper class");
     }
 
-
     /**
-     * {@link Inventory} from {@link InventoryDto}
+     * {@link Inventory} from {@link InventoryDTO}
      *
-     * @param dto
+     * @param dto {@link InventoryDTO}
      * @return {@link Inventory}
      */
-    public static Inventory from(InventoryDto dto) {
-        Inventory inventory = new Inventory(dto.getId());
+    public static Inventory full(InventoryDTO dto) {
+        Inventory inventory = new Inventory();
+        inventory.setId(dto.getId());
         inventory.setQuantity(dto.getQuantity());
         inventory.setStatus(dto.getStatus());
-        inventory.setProduct(ProductMapper.from(dto.getProduct()));
-        inventory.setUser(UserMapper.from(dto.getUser()));
+        inventory.setDescription(dto.getDescription());
+        inventory.setProduct(ProductMapper.full(dto.getProduct()));
+        inventory.setUser(UserMapper.full(dto.getUser()));
         return inventory;
     }
 
     /**
-     * {@link InventoryDto} from {@link Inventory}
+     * {@link InventoryDTO} from {@link Inventory}
      *
-     * @param inventory
-     * @return {@link InventoryDto}
+     * @param inventory {@link Inventory}
+     * @return {@link InventoryDTO}
      */
-    public static InventoryDto from(Inventory inventory) {
-        InventoryDto dto = new InventoryDto(inventory.getId());
-        dto.setQuantity(inventory.getQuantity());
-        dto.setStatus(inventory.getStatus());
-        dto.setDescription(inventory.getDescription());
-        dto.setProduct(ProductMapper.from(inventory.getProduct()));
-        dto.setUser(UserMapper.onlyId(inventory.getUser()));
-        return dto;
+    public static InventoryDTO full(Inventory inventory) {
+        if (inventory == null) return null;
+
+        return InventoryDTO.builder()
+                .id(inventory.getId())
+                .quantity(inventory.getQuantity())
+                .status(inventory.getStatus())
+                .description(inventory.getDescription())
+                .product(ProductMapper.base(inventory.getProduct()))
+                .user(UserMapper.onlyId(inventory.getUser()))
+                .build();
     }
 }

src/main/java/com/dev/servlet/mapper/ProductMapper.java
@@ -1,49 +1,74 @@
 package com.dev.servlet.mapper;
 
-import com.dev.servlet.dto.ProductDto;
+import com.dev.servlet.dto.ProductDTO;
 import com.dev.servlet.pojo.Product;
 
 public final class ProductMapper {
+
     private ProductMapper() {
+        throw new IllegalStateException("ProductMapper class");
     }
 
     /**
-     * {@link ProductDto} from {@link Product}
+     * {@link ProductDTO} from {@link Product}
      *
-     * @param product
-     * @return {@link ProductDto}
+     * @param product {@link Product}
+     * @return {@link ProductDTO}
      */
-    public static ProductDto from(Product product) {
+    public static ProductDTO full(Product product) {
         if (product == null) return null;
-        ProductDto dto = new ProductDto(product.getId());
-        dto.setName(product.getName());
-        dto.setStatus(product.getStatus());
-        dto.setRegisterDate(product.getRegisterDate());
-        dto.setUrl(product.getUrl());
-        dto.setUser(UserMapper.onlyId(product.getUser()));
-        dto.setPrice(product.getPrice());
-        dto.setDescription(product.getDescription());
-        dto.setCategory(CategoryMapper.onlyId(product.getCategory()));
-        return dto;
+
+        return ProductDTO.builder()
+                .id(product.getId())
+                .name(product.getName())
+                .status(product.getStatus())
+                .registerDate(product.getRegisterDate())
+                .url(product.getUrl())
+                .price(product.getPrice())
+                .description(product.getDescription())
+                .user(UserMapper.onlyId(product.getUser()))
+                .category(CategoryMapper.onlyId(product.getCategory()))
+                .build();
     }
 
     /**
-     * {@link Product} from {@link ProductDto}
+     * {@link ProductDTO} from {@link Product}
      *
-     * @param dto
+     * @param product {@link Product}
+     * @return {@link ProductDTO}
+     */
+    public static ProductDTO base(Product product) {
+        if (product == null) return null;
+
+        return ProductDTO.builder()
+                .id(product.getId())
+                .name(product.getName())
+                .status(product.getStatus())
+                .registerDate(product.getRegisterDate())
+                .url(product.getUrl())
+                .price(product.getPrice())
+                .description(product.getDescription())
+                .build();
+    }
+
+    /**
+     * {@link Product} from {@link ProductDTO}
+     *
+     * @param dto {@link ProductDTO}
      * @return {@link Product}
      */
-    public static Product from(ProductDto dto) {
+    public static Product full(ProductDTO dto) {
         if (dto == null) return null;
-        Product product = new Product(dto.getId());
+
+        Product product = new Product();
+        product.setId(dto.getId());
         product.setName(dto.getName());
         product.setStatus(dto.getStatus());
         product.setRegisterDate(dto.getRegisterDate());
         product.setUrl(dto.getUrl());
         product.setPrice(dto.getPrice());
         product.setDescription(dto.getDescription());
-
-        product.setUser(UserMapper.from(dto.getUser()));
+        product.setUser(UserMapper.full(dto.getUser()));
         product.setCategory(CategoryMapper.from(dto.getCategory()));
         return product;
     }

src/main/webapp/WEB-INF/view/pages/category/formListCategory.jsp
@@ -1,30 +1,42 @@
-<%@ include file="/WEB-INF/jspf/common-imports.jspf" %>
+<%@ include file="/WEB-INF/routes/category-routes.jspf" %>
+<%@ page import="com.dev.servlet.interfaces.IHttpResponse" %>
 <jsp:include page="/WEB-INF/view/components/header.jsp"/>
 
-<div class="main">
-    <form>
-        <div class="col-md-6">
-            <div class="mb-3">
-                <label for="inputId" class="form-label">ID</label>
-                <input type="text" name="id" class="form-control col-md-3" id="inputId"
-                       value="${ category.id }" readonly="readonly"/>
-            </div>
-            <div class="mb-3">
-                <label for="inputName" class="form-label">NAME</label>
-                <input type="text" name="name" class="form-control" id="inputName"
-                       placeholder="name" value="${ category.name }" autocomplete="name" required minlength="4"
-                       readonly="readonly"/>
-            </div>
+<%
+    request.setAttribute("category", ((IHttpResponse<?>) request.getAttribute("response")).getResponse());
+%>
 
-            <!-- action -->
+<div class="main">
+    <div class="col-md-6">
+        <div class="mb-3">
+            <label for="inputId" class="form-label">ID</label>
+            <input type="text" name="id" class="form-control col-md-3" id="inputId"
+                   value="${ category.id }" readonly="readonly"/>
+        </div>
+        <div class="mb-3">
+            <label for="inputName" class="form-label">NAME</label>
+            <input type="text" name="name" class="form-control" id="inputName"
+                   placeholder="name" value="${ category.name }" autocomplete="name" required minlength="4"
+                   readonly="readonly"/>
+        </div>
+
+        <div class="align-end">
+            <jsp:include page="/WEB-INF/view/components/buttons/customButton.jsp">
+                <jsp:param name="btnLabel" value="Back"/>
+                <jsp:param name="btnType" value="button"/>
+                <jsp:param name="btnClass" value="btn btn-light"/>
+                <jsp:param name="btnIcon" value="bi bi-arrow-left"/>
+                <jsp:param name="btnOnclick" value="onclick='window.location.href=`${ listCategory }`'"/>
+                <jsp:param name="btnId" value="id='backButton'"/>
+            </jsp:include>
+
+            <span class="mr-2"></span>
+
             <a type="button" href="${ editCategory }/${ category.id }" class="btn btn-success">
                 Edit <i class="bi bi-pencil-square"></i>
             </a>
-            <a type="button" href="${ listCategories }" class="btn btn-light">
-                Go back
-            </a>
         </div>
-    </form>
+    </div>
 </div>
 <!-- footer -->
 <jsp:include page="/WEB-INF/view/components/footer.jsp"/>

src/main/webapp/WEB-INF/view/pages/category/formUpdateCategory.jsp
@@ -1,5 +1,11 @@
-<%@ include file="/WEB-INF/jspf/common-imports.jspf" %>
+<%@ include file="/WEB-INF/routes/category-routes.jspf" %>
+<%@ page import="com.dev.servlet.interfaces.IHttpResponse" %>
 <jsp:include page="/WEB-INF/view/components/header.jsp"/>
+
+<%
+    request.setAttribute("category", ((IHttpResponse<?>) request.getAttribute("response")).getResponse());
+%>
+
 
 <div class="main">
     <form action="${ updateCategory }/${category.id}" method="post">
@@ -16,12 +22,11 @@
                        placeholder="name" value="${ category.name }" autocomplete="name" required minlength="4"/>
             </div>
 
-            <!-- action -->
-
-            <button type="submit" class="btn btn-primary">Save</button>
-            <a type="button" href="${ listCategories }/${category.id }" class="btn btn-light">
-                Cancel
-            </a>
+            <div class="align-end">
+                <jsp:include page="/WEB-INF/view/components/buttons/backButton.jsp"/>
+                <span class="mr-2"></span>
+                <jsp:include page="/WEB-INF/view/components/buttons/saveButton.jsp"/>
+            </div>
         </div>
     </form>
 </div>

m-feliciano/javaee-framework

src/main/java/com/dev/servlet/mapper/InventoryMapper.java
@@ -1,41 +1,47 @@
 package com.dev.servlet.mapper;
 
-import com.dev.servlet.dto.InventoryDto;
+import com.dev.servlet.dto.InventoryDTO;
 import com.dev.servlet.pojo.Inventory;
 
 public final class InventoryMapper {
+
     private InventoryMapper() {
+        throw new IllegalStateException("InventoryMapper class");
     }
 
-
     /**
-     * {@link Inventory} from {@link InventoryDto}
+     * {@linkplain Inventory} from {@linkplain InventoryDTO}
      *
-     * @param dto
-     * @return {@link Inventory}
+     * @param dto {@linkplain InventoryDTO}
+     * @return {@linkplain Inventory}
      */
-    public static Inventory from(InventoryDto dto) {
-        Inventory inventory = new Inventory(dto.getId());
+    public static Inventory full(InventoryDTO dto) {
+        Inventory inventory = new Inventory();
+        inventory.setId(dto.getId());
         inventory.setQuantity(dto.getQuantity());
         inventory.setStatus(dto.getStatus());
-        inventory.setProduct(ProductMapper.from(dto.getProduct()));
-        inventory.setUser(UserMapper.from(dto.getUser()));
+        inventory.setDescription(dto.getDescription());
+        inventory.setProduct(ProductMapper.base(dto.getProduct()));
+        inventory.setUser(UserMapper.onlyId(dto.getUser()));
         return inventory;
     }
 
     /**
-     * {@link InventoryDto} from {@link Inventory}
+     * {@linkplain InventoryDTO} from {@linkplain Inventory}
      *
-     * @param inventory
-     * @return {@link InventoryDto}
+     * @param inventory {@linkplain Inventory}
+     * @return {@linkplain InventoryDTO}
      */
-    public static InventoryDto from(Inventory inventory) {
-        InventoryDto dto = new InventoryDto(inventory.getId());
-        dto.setQuantity(inventory.getQuantity());
-        dto.setStatus(inventory.getStatus());
-        dto.setDescription(inventory.getDescription());
-        dto.setProduct(ProductMapper.from(inventory.getProduct()));
-        dto.setUser(UserMapper.onlyId(inventory.getUser()));
-        return dto;
+    public static InventoryDTO full(Inventory inventory) {
+        if (inventory == null) return null;
+
+        return InventoryDTO.builder()
+                .id(inventory.getId())
+                .quantity(inventory.getQuantity())
+                .status(inventory.getStatus())
+                .description(inventory.getDescription())
+                .product(ProductMapper.base(inventory.getProduct()))
+                .user(UserMapper.onlyId(inventory.getUser()))
+                .build();
     }
 }

src/main/java/com/dev/servlet/mapper/ProductMapper.java
@@ -1,49 +1,94 @@
 package com.dev.servlet.mapper;
 
-import com.dev.servlet.dto.ProductDto;
+import com.dev.servlet.dto.ProductDTO;
 import com.dev.servlet.pojo.Product;
 
 public final class ProductMapper {
+
     private ProductMapper() {
+        throw new IllegalStateException("ProductMapper class");
     }
 
     /**
-     * {@link ProductDto} from {@link Product}
+     * {@linkplain ProductDTO} from {@linkplain Product}
      *
-     * @param product
-     * @return {@link ProductDto}
+     * @param product {@linkplain Product}
+     * @return {@linkplain ProductDTO}
      */
-    public static ProductDto from(Product product) {
+    public static ProductDTO full(Product product) {
         if (product == null) return null;
-        ProductDto dto = new ProductDto(product.getId());
-        dto.setName(product.getName());
-        dto.setStatus(product.getStatus());
-        dto.setRegisterDate(product.getRegisterDate());
-        dto.setUrl(product.getUrl());
-        dto.setUser(UserMapper.onlyId(product.getUser()));
-        dto.setPrice(product.getPrice());
-        dto.setDescription(product.getDescription());
-        dto.setCategory(CategoryMapper.onlyId(product.getCategory()));
-        return dto;
+
+        return ProductDTO.builder()
+                .id(product.getId())
+                .name(product.getName())
+                .status(product.getStatus())
+                .registerDate(product.getRegisterDate())
+                .url(product.getUrl())
+                .price(product.getPrice())
+                .description(product.getDescription())
+                .user(UserMapper.onlyId(product.getUser()))
+                .category(CategoryMapper.onlyId(product.getCategory()))
+                .build();
     }
 
     /**
-     * {@link Product} from {@link ProductDto}
+     * {@linkplain ProductDTO} from {@linkplain Product}
      *
-     * @param dto
-     * @return {@link Product}
+     * @param product {@linkplain Product}
+     * @return {@linkplain ProductDTO}
      */
-    public static Product from(ProductDto dto) {
+    public static ProductDTO base(Product product) {
+        if (product == null) return null;
+
+        return ProductDTO.builder()
+                .id(product.getId())
+                .name(product.getName())
+                .status(product.getStatus())
+                .registerDate(product.getRegisterDate())
+                .url(product.getUrl())
+                .price(product.getPrice())
+                .description(product.getDescription())
+                .build();
+    }
+
+    /**
+     * {@linkplain ProductDTO} from {@linkplain Product}
+     *
+     * @param productDTO {@linkplain Product}
+     * @return {@linkplain ProductDTO}
+     */
+    public static Product base(ProductDTO productDTO) {
+        if (productDTO == null) return null;
+
+        Product product = new Product();
+        product.setId(product.getId());
+        product.setName(product.getName());
+        product.setStatus(product.getStatus());
+        product.setRegisterDate(product.getRegisterDate());
+        product.setUrl(product.getUrl());
+        product.setPrice(product.getPrice());
+        product.setDescription(product.getDescription());
+        return product;
+    }
+
+    /**
+     * {@linkplain Product} from {@linkplain ProductDTO}
+     *
+     * @param dto {@linkplain ProductDTO}
+     * @return {@linkplain Product}
+     */
+    public static Product full(ProductDTO dto) {
         if (dto == null) return null;
-        Product product = new Product(dto.getId());
+
+        Product product = new Product();
+        product.setId(dto.getId());
         product.setName(dto.getName());
         product.setStatus(dto.getStatus());
         product.setRegisterDate(dto.getRegisterDate());
         product.setUrl(dto.getUrl());
         product.setPrice(dto.getPrice());
         product.setDescription(dto.getDescription());
-
-        product.setUser(UserMapper.from(dto.getUser()));
+        product.setUser(UserMapper.onlyId(dto.getUser()));
         product.setCategory(CategoryMapper.from(dto.getCategory()));
         return product;
     }

src/main/webapp/WEB-INF/view/pages/category/formListCategory.jsp
@@ -1,30 +1,42 @@
-<%@ include file="/WEB-INF/jspf/common-imports.jspf" %>
+<%@ include file="/WEB-INF/routes/category-routes.jspf" %>
+<%@ page import="com.dev.servlet.interfaces.IHttpResponse" %>
 <jsp:include page="/WEB-INF/view/components/header.jsp"/>
 
-<div class="main">
-    <form>
-        <div class="col-md-6">
-            <div class="mb-3">
-                <label for="inputId" class="form-label">ID</label>
-                <input type="text" name="id" class="form-control col-md-3" id="inputId"
-                       value="${ category.id }" readonly="readonly"/>
-            </div>
-            <div class="mb-3">
-                <label for="inputName" class="form-label">NAME</label>
-                <input type="text" name="name" class="form-control" id="inputName"
-                       placeholder="name" value="${ category.name }" autocomplete="name" required minlength="4"
-                       readonly="readonly"/>
-            </div>
+<%
+    request.setAttribute("category", ((IHttpResponse<?>) request.getAttribute("response")).body());
+%>
 
-            <!-- action -->
+<div class="main">
+    <div class="col-md-6">
+        <div class="mb-3">
+            <label for="inputId" class="form-label">ID</label>
+            <input type="text" name="id" class="form-control col-md-3" id="inputId"
+                   value="${ category.id }" readonly="readonly"/>
+        </div>
+        <div class="mb-3">
+            <label for="inputName" class="form-label">NAME</label>
+            <input type="text" name="name" class="form-control" id="inputName"
+                   placeholder="name" value="${ category.name }" autocomplete="name" required minlength="4"
+                   readonly="readonly"/>
+        </div>
+
+        <div class="align-end">
+            <jsp:include page="/WEB-INF/view/components/buttons/customButton.jsp">
+                <jsp:param name="btnLabel" value="Back"/>
+                <jsp:param name="btnType" value="button"/>
+                <jsp:param name="btnClass" value="btn btn-light"/>
+                <jsp:param name="btnIcon" value="bi bi-arrow-left"/>
+                <jsp:param name="btnOnclick" value="onclick='window.location.href=`${ listCategory }`'"/>
+                <jsp:param name="btnId" value="id='backButton'"/>
+            </jsp:include>
+
+            <span class="mr-2"></span>
+
             <a type="button" href="${ editCategory }/${ category.id }" class="btn btn-success">
                 Edit <i class="bi bi-pencil-square"></i>
             </a>
-            <a type="button" href="${ listCategories }" class="btn btn-light">
-                Go back
-            </a>
         </div>
-    </form>
+    </div>
 </div>
 <!-- footer -->
 <jsp:include page="/WEB-INF/view/components/footer.jsp"/>

src/main/webapp/WEB-INF/view/pages/category/formUpdateCategory.jsp
@@ -1,5 +1,11 @@
-<%@ include file="/WEB-INF/jspf/common-imports.jspf" %>
+<%@ include file="/WEB-INF/routes/category-routes.jspf" %>
+<%@ page import="com.dev.servlet.interfaces.IHttpResponse" %>
 <jsp:include page="/WEB-INF/view/components/header.jsp"/>
+
+<%
+    request.setAttribute("category", ((IHttpResponse<?>) request.getAttribute("response")).body());
+%>
+
 
 <div class="main">
     <form action="${ updateCategory }/${category.id}" method="post">
@@ -16,12 +22,11 @@
                        placeholder="name" value="${ category.name }" autocomplete="name" required minlength="4"/>
             </div>
 
-            <!-- action -->
-
-            <button type="submit" class="btn btn-primary">Save</button>
-            <a type="button" href="${ listCategories }/${category.id }" class="btn btn-light">
-                Cancel
-            </a>
+            <div class="align-end">
+                <jsp:include page="/WEB-INF/view/components/buttons/backButton.jsp"/>
+                <span class="mr-2"></span>
+                <jsp:include page="/WEB-INF/view/components/buttons/saveButton.jsp"/>
+            </div>
         </div>
     </form>
 </div>