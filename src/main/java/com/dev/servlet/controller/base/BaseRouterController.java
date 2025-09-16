m-feliciano/servlets

v2.0: Refactor the project structure and enhance the pagination system
AI summary:

Restructured package hierarchy for better organization and readability, moving key parts to the "persistence" and "model" packages. Enhanced the pagination system by introducing `IPageRequest` and `IPageable`, improved query building, and updated DAO methods to handle filtering and sorting efficiently.

src/main/java/com/dev/servlet/controller/base/BaseRouterController.java
@@ -1,0 +1,96 @@
+package com.dev.servlet.controller.base;
+
+import com.dev.servlet.domain.transfer.request.Request;
+import com.dev.servlet.domain.transfer.response.IHttpResponse;
+import com.dev.servlet.core.annotation.Property;
+import com.dev.servlet.core.annotation.RequestMapping;
+import com.dev.servlet.core.exception.ServiceException;
+import com.dev.servlet.core.util.EndpointParser;
+import com.dev.servlet.core.util.PropertiesUtil;
+import com.dev.servlet.core.validator.RequestValidator;
+
+import javax.servlet.http.HttpServletResponse;
+import java.lang.reflect.Method;
+import java.lang.reflect.Parameter;
+import java.util.HashMap;
+import java.util.Map;
+
+import static com.dev.servlet.core.util.ThrowableUtils.throwServiceError;
+
+public abstract class BaseRouterController {
+    private final Map<String, RouteMapping> routeMappings = new HashMap<>();
+
+    public record RouteMapping(Method method, Class<?>[] parameterTypes) {
+        public RouteMapping(Method method) {
+            this(method, method.getParameterTypes());
+        }
+    }
+
+    protected BaseRouterController() {
+        initRouteMapping();
+    }
+
+    private void initRouteMapping() {
+        for (Method method : this.getClass().getDeclaredMethods()) {
+            if (!method.isAnnotationPresent(RequestMapping.class)) {
+                continue;
+            }
+            var requestMapping = method.getAnnotation(RequestMapping.class);
+            String serviceController = requestMapping.value().substring(1);
+            routeMappings.put(serviceController, new RouteMapping(method));
+        }
+    }
+
+    public <U> IHttpResponse<U> route(EndpointParser endpoint, Request request) throws Exception {
+        var routeMapping = routeMappingFromEndpoint(endpoint);
+        var requestMapping = routeMapping.method().getAnnotation(RequestMapping.class);
+        RequestValidator.validate(endpoint, requestMapping, request);
+        Object[] args = prepareMethodArguments(routeMapping, request);
+        return invokeServiceMethod(this, routeMapping.method(), args);
+    }
+
+    private RouteMapping routeMappingFromEndpoint(EndpointParser endpoint) throws ServiceException {
+        var routeMapping = routeMappings.get(endpoint.getEndpoint());
+        if (routeMapping == null) {
+            throwServiceError(HttpServletResponse.SC_NOT_IMPLEMENTED, "Endpoint not implemented: " + endpoint.getEndpoint());
+        }
+        return routeMapping;
+    }
+
+    private Object[] prepareMethodArguments(RouteMapping mapping, Request request) throws ServiceException {
+        Method method = mapping.method();
+        Parameter[] parameters = method.getParameters();
+        Object[] args = new Object[mapping.parameterTypes().length];
+        for (int i = 0; i < mapping.parameterTypes().length; i++) {
+            Parameter parameter = parameters[i];
+            if (parameter.isAnnotationPresent(Property.class)) {
+                String propertyKey = parameter.getAnnotation(Property.class).value();
+                args[i] = PropertiesUtil.getProperty(propertyKey, "");
+            } else {
+                Class<?> parameterType = mapping.parameterTypes()[i];
+                args[i] = resolveArgument(parameterType, request);
+            }
+        }
+        return args;
+    }
+
+    private Object resolveArgument(Class<?> parameter, Request request) throws ServiceException {
+        if (Request.class.isAssignableFrom(parameter)) {
+            return request;
+        }
+        if (request.getBody() != null) {
+            return request.getBody()
+                    .stream()
+                    .filter(body -> parameter.isAssignableFrom(body.getClass()))
+                    .findFirst()
+                    .orElse(null);
+        }
+        return null;
+    }
+
+    private <U> IHttpResponse<U> invokeServiceMethod(Object instance, Method method, Object[] args) throws Exception {
+        @SuppressWarnings("ALL")
+        var response = (IHttpResponse<U>) method.invoke(instance, args);
+        return response;
+    }
+}

src/main/java/com/dev/servlet/domain/transfer/dto/ProductDTO.java
@@ -1,0 +1,27 @@
+package com.dev.servlet.domain.transfer.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class ProductDTO extends DataTransferObject<Long> {
+    private Long id;
+    private String name;
+    private String description;
+    private String url;
+    private Date registerDate;
+    private BigDecimal price;
+    private UserDTO user;
+    private String status;
+    private CategoryDTO category;
+}

src/test/java/com/dev/servlet/controller/ProductControllerTest.java
@@ -1,0 +1,217 @@
+package com.dev.servlet.controller;
+
+import com.dev.servlet.domain.transfer.dto.CategoryDTO;
+import com.dev.servlet.domain.transfer.dto.ProductDTO;
+import com.dev.servlet.domain.transfer.records.Query;
+import com.dev.servlet.domain.transfer.records.Sort;
+import com.dev.servlet.domain.transfer.request.Request;
+import com.dev.servlet.domain.transfer.response.IHttpResponse;
+import com.dev.servlet.domain.transfer.response.IServletResponse;
+import com.dev.servlet.core.exception.ServiceException;
+import com.dev.servlet.core.mapper.Mapper;
+import com.dev.servlet.core.mapper.ProductMapper;
+import com.dev.servlet.domain.model.Product;
+import com.dev.servlet.domain.service.ICategoryService;
+import com.dev.servlet.domain.service.IProductService;
+import com.dev.servlet.infrastructure.persistence.IPageable;
+import com.dev.servlet.infrastructure.persistence.internal.PageRequest;
+import com.dev.servlet.infrastructure.persistence.internal.PageResponse;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.math.BigDecimal;
+import java.util.Collection;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+class ProductControllerTest {
+
+    @Mock
+    private IProductService productService;
+
+    @Mock
+    private ICategoryService categoryService;
+
+    @InjectMocks
+    private ProductController productController;
+
+    @Mock
+    private Request request;
+
+    @Mock
+    private PageRequest<Object> pageRequest;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+
+        when(request.getToken()).thenReturn("fakeToken");
+        when(request.getQuery()).thenReturn(mock(Query.class));
+        when(request.getQuery().getPageRequest()).thenReturn(pageRequest);
+//        when(request.query().getPageRequestImpl().getRecords()).thenReturn(List.of(1L, 2L));
+    }
+
+    @Test
+    @DisplayName(
+            "Test create method to add a new product. " +
+            "It should return a 201 status code and the expected response.")
+    void testCreateProduct() {
+        ProductDTO productDTO = new ProductDTO();
+        productDTO.setId(1L);
+
+        when(productService.create(request)).thenReturn(productDTO);
+
+        IHttpResponse<Void> response = productController.create(request);
+
+        assertNotNull(response);
+        assertEquals(201, response.statusCode());
+        verify(productService, times(1)).create(request);
+    }
+
+    @Test
+    @DisplayName(
+            "Test forward method to create form. " +
+            "It should return a 302 status code and the expected response.")
+    void testForwardToCreateForm() {
+        Collection<CategoryDTO> categories = List.of(new CategoryDTO());
+        when(categoryService.list(any())).thenReturn(categories);
+
+        IHttpResponse<Collection<CategoryDTO>> response = productController.forward(request);
+
+        assertNotNull(response);
+        assertEquals(302, response.statusCode());
+        assertEquals(categories, response.body());
+        verify(categoryService, times(1)).list(any());
+    }
+
+    @Test
+    @DisplayName(
+            "Test edit method to update a product. " +
+            "It should return a 200 status code and the expected response.")
+    void testEditProduct() throws ServiceException {
+        ProductDTO productDTO = new ProductDTO();
+        Collection<CategoryDTO> categories = List.of(new CategoryDTO());
+
+        when(categoryService.list(any())).thenReturn(categories);
+        when(productService.findById(request)).thenReturn(productDTO);
+
+        IServletResponse response = productController.edit(request);
+
+        assertNotNull(response);
+        verify(productService, times(1)).findById(request);
+        verify(categoryService, times(1)).list(any());
+    }
+
+    @Test
+    @DisplayName(
+            "Test listProducts method to retrieve a list of products. " +
+            "It should return a 200 status code and the expected response.")
+    @SuppressWarnings("unchecked")
+    void testListProducts() {
+        // Setup
+        Product filterMock = new Product("prod", "desc", null);
+        when(productService.getEntity(any())).thenReturn(filterMock);
+
+        var categories = List.of(new CategoryDTO());
+        when(categoryService.list(any())).thenReturn(categories);
+
+        var products = List.of(
+                ProductMapper.base(new Product("prod1", "desc1", BigDecimal.valueOf(50))),
+                ProductMapper.base(new Product("prod2", "desc2", BigDecimal.valueOf(50)))
+        );
+
+        var pageableMock = PageResponse.<ProductDTO>builder()
+                .content(products)
+                .currentPage(1)
+                .pageSize(2)
+                .sort(Sort.by("id").ascending())
+                .build();
+
+        when(productService.getAllPageable(any(), any(Mapper.class))).thenReturn(pageableMock);
+        when(productService.calculateTotalPriceFor(any())).thenReturn(BigDecimal.valueOf(100));
+
+        // Execution
+        IServletResponse response = productController.list(request);
+
+        // Verification
+        assertNotNull(response);
+        assertEquals(200, response.statusCode());
+
+        // Verify pageable content
+        var pageable = response.body().stream()
+                .filter(pair -> "pageable".equals(pair.key()))
+                .findFirst()
+                .map(e -> (IPageable<ProductDTO>) e.value())
+                .orElseThrow(() -> new AssertionError("Pageable not found"));
+
+        long counter = pageable.getContent().size();
+        assertEquals(2, counter);
+
+        // Verify total price
+        BigDecimal totalPrice = response.body().stream()
+                .filter(pair -> "totalPrice".equals(pair.key()))
+                .findFirst()
+                .map(e -> (BigDecimal) e.value())
+                .orElse(null);
+
+        assertEquals(BigDecimal.valueOf(100), totalPrice);
+
+        // Verify categories
+        var responseCategories = response.body().stream()
+                .filter(pair -> "categories".equals(pair.key()))
+                .findFirst()
+                .map(e -> (Collection<CategoryDTO>) e.value())
+                .orElse(null);
+
+        assertEquals(categories, responseCategories);
+
+        // Verify interactions
+        verify(productService, times(1)).getEntity(request);
+        verify(productService, times(1)).getAllPageable(any(), any(Mapper.class));
+        verify(productService, times(1)).calculateTotalPriceFor(filterMock);
+        verify(categoryService, times(1)).list(any());
+    }
+
+    @Test
+    @DisplayName(
+            "Test delete method to remove a product. " +
+            "It should return a 200 status code and the expected response.")
+    void testDeleteProduct() throws ServiceException {
+        doReturn(true).when(productService).delete(request);
+
+        IHttpResponse<Void> response = productController.delete(request);
+
+        assertNotNull(response);
+        assertEquals(200, response.statusCode());
+        verify(productService, times(1)).delete(request);
+    }
+
+    @Test
+    @DisplayName(
+            "Test listById method to retrieve a product by ID. " +
+            "It should return a 200 status code and the expected response.")
+    void testGetById() throws ServiceException {
+        ProductDTO productDTO = new ProductDTO();
+        productDTO.setId(1L);
+
+        when(productService.findById(request)).thenReturn(productDTO);
+
+        IHttpResponse<ProductDTO> response = productController.getById(request);
+        assertNotNull(response);
+        assertEquals(200, response.statusCode());
+        assertEquals(productDTO, response.body());
+        verify(productService, times(1)).findById(request);
+    }
+}

m-feliciano/javaee-framework

src/main/java/com/dev/servlet/controller/base/BaseRouterController.java
@@ -1,0 +1,96 @@
+package com.dev.servlet.controller.base;
+
+import com.dev.servlet.domain.transfer.request.Request;
+import com.dev.servlet.domain.transfer.response.IHttpResponse;
+import com.dev.servlet.core.annotation.Property;
+import com.dev.servlet.core.annotation.RequestMapping;
+import com.dev.servlet.core.exception.ServiceException;
+import com.dev.servlet.core.util.EndpointParser;
+import com.dev.servlet.core.util.PropertiesUtil;
+import com.dev.servlet.core.validator.RequestValidator;
+
+import javax.servlet.http.HttpServletResponse;
+import java.lang.reflect.Method;
+import java.lang.reflect.Parameter;
+import java.util.HashMap;
+import java.util.Map;
+
+import static com.dev.servlet.core.util.ThrowableUtils.serviceError;
+
+public abstract class BaseRouterController {
+    private final Map<String, RouteMapping> routeMappings = new HashMap<>();
+
+    public record RouteMapping(Method method, Class<?>[] parameterTypes) {
+        public RouteMapping(Method method) {
+            this(method, method.getParameterTypes());
+        }
+    }
+
+    protected BaseRouterController() {
+        initRouteMapping();
+    }
+
+    private void initRouteMapping() {
+        for (Method method : this.getClass().getDeclaredMethods()) {
+            if (!method.isAnnotationPresent(RequestMapping.class)) {
+                continue;
+            }
+            var requestMapping = method.getAnnotation(RequestMapping.class);
+            String serviceController = requestMapping.value().substring(1);
+            routeMappings.put(serviceController, new RouteMapping(method));
+        }
+    }
+
+    public <U> IHttpResponse<U> route(EndpointParser endpoint, Request request) throws Exception {
+        var routeMapping = routeMappingFromEndpoint(endpoint);
+        var requestMapping = routeMapping.method().getAnnotation(RequestMapping.class);
+        RequestValidator.validate(endpoint, requestMapping, request);
+        Object[] args = prepareMethodArguments(routeMapping, request);
+        return invokeServiceMethod(this, routeMapping.method(), args);
+    }
+
+    private RouteMapping routeMappingFromEndpoint(EndpointParser endpoint) throws ServiceException {
+        var routeMapping = routeMappings.get(endpoint.getEndpoint());
+        if (routeMapping == null) {
+            throw serviceError(HttpServletResponse.SC_NOT_IMPLEMENTED, "Endpoint not implemented: " + endpoint.getEndpoint());
+        }
+        return routeMapping;
+    }
+
+    private Object[] prepareMethodArguments(RouteMapping mapping, Request request) throws ServiceException {
+        Method method = mapping.method();
+        Parameter[] parameters = method.getParameters();
+        Object[] args = new Object[mapping.parameterTypes().length];
+        for (int i = 0; i < mapping.parameterTypes().length; i++) {
+            Parameter parameter = parameters[i];
+            if (parameter.isAnnotationPresent(Property.class)) {
+                String propertyKey = parameter.getAnnotation(Property.class).value();
+                args[i] = PropertiesUtil.getProperty(propertyKey, "");
+            } else {
+                Class<?> parameterType = mapping.parameterTypes()[i];
+                args[i] = resolveArgument(parameterType, request);
+            }
+        }
+        return args;
+    }
+
+    private Object resolveArgument(Class<?> parameter, Request request) throws ServiceException {
+        if (Request.class.isAssignableFrom(parameter)) {
+            return request;
+        }
+        if (request.getBody() != null) {
+            return request.getBody()
+                    .stream()
+                    .filter(body -> parameter.isAssignableFrom(body.getClass()))
+                    .findFirst()
+                    .orElse(null);
+        }
+        return null;
+    }
+
+    private <U> IHttpResponse<U> invokeServiceMethod(Object instance, Method method, Object[] args) throws Exception {
+        @SuppressWarnings("ALL")
+        var response = (IHttpResponse<U>) method.invoke(instance, args);
+        return response;
+    }
+}

src/main/java/com/dev/servlet/domain/transfer/dto/ProductDTO.java
@@ -1,0 +1,27 @@
+package com.dev.servlet.domain.transfer.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class ProductDTO extends DataTransferObject<String> {
+    private String id;
+    private String name;
+    private String description;
+    private String url;
+    private Date registerDate;
+    private BigDecimal price;
+    private UserDTO user;
+    private String status;
+    private CategoryDTO category;
+}

src/test/java/com/dev/servlet/controller/ProductControllerTest.java
@@ -1,0 +1,218 @@
+package com.dev.servlet.controller;
+
+import com.dev.servlet.domain.transfer.dto.CategoryDTO;
+import com.dev.servlet.domain.transfer.dto.ProductDTO;
+import com.dev.servlet.domain.transfer.records.Query;
+import com.dev.servlet.domain.transfer.records.Sort;
+import com.dev.servlet.domain.transfer.request.Request;
+import com.dev.servlet.domain.transfer.response.IHttpResponse;
+import com.dev.servlet.domain.transfer.response.IServletResponse;
+import com.dev.servlet.core.exception.ServiceException;
+import com.dev.servlet.core.mapper.Mapper;
+import com.dev.servlet.core.mapper.ProductMapper;
+import com.dev.servlet.domain.model.Product;
+import com.dev.servlet.domain.service.ICategoryService;
+import com.dev.servlet.domain.service.IProductService;
+import com.dev.servlet.infrastructure.persistence.IPageable;
+import com.dev.servlet.infrastructure.persistence.internal.PageRequest;
+import com.dev.servlet.infrastructure.persistence.internal.PageResponse;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.math.BigDecimal;
+import java.util.Collection;
+import java.util.List;
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+class ProductControllerTest {
+
+    @Mock
+    private IProductService productService;
+
+    @Mock
+    private ICategoryService categoryService;
+
+    @InjectMocks
+    private ProductController productController;
+
+    @Mock
+    private Request request;
+
+    @Mock
+    private PageRequest<Object> pageRequest;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+
+        when(request.getToken()).thenReturn("fakeToken");
+        when(request.getQuery()).thenReturn(mock(Query.class));
+        when(request.getQuery().getPageRequest()).thenReturn(pageRequest);
+//        when(request.query().getPageRequestImpl().getRecords()).thenReturn(List.of(1L, 2L));
+    }
+
+    @Test
+    @DisplayName(
+            "Test create method to add a new product. " +
+            "It should return a 201 status code and the expected response.")
+    void testCreateProduct() {
+        ProductDTO productDTO = new ProductDTO();
+        productDTO.setId(UUID.randomUUID().toString());
+
+        when(productService.create(request)).thenReturn(productDTO);
+
+        IHttpResponse<Void> response = productController.create(request);
+
+        assertNotNull(response);
+        assertEquals(201, response.statusCode());
+        verify(productService, times(1)).create(request);
+    }
+
+    @Test
+    @DisplayName(
+            "Test forward method to create form. " +
+            "It should return a 302 status code and the expected response.")
+    void testForwardToCreateForm() {
+        Collection<CategoryDTO> categories = List.of(new CategoryDTO());
+        when(categoryService.list(any())).thenReturn(categories);
+
+        IHttpResponse<Collection<CategoryDTO>> response = productController.forward(request);
+
+        assertNotNull(response);
+        assertEquals(302, response.statusCode());
+        assertEquals(categories, response.body());
+        verify(categoryService, times(1)).list(any());
+    }
+
+    @Test
+    @DisplayName(
+            "Test edit method to update a product. " +
+            "It should return a 200 status code and the expected response.")
+    void testEditProduct() throws ServiceException {
+        ProductDTO productDTO = new ProductDTO();
+        Collection<CategoryDTO> categories = List.of(new CategoryDTO());
+
+        when(categoryService.list(any())).thenReturn(categories);
+        when(productService.findById(request)).thenReturn(productDTO);
+
+        IServletResponse response = productController.edit(request);
+
+        assertNotNull(response);
+        verify(productService, times(1)).findById(request);
+        verify(categoryService, times(1)).list(any());
+    }
+
+    @Test
+    @DisplayName(
+            "Test listProducts method to retrieve a list of products. " +
+            "It should return a 200 status code and the expected response.")
+    @SuppressWarnings("unchecked")
+    void testListProducts() {
+        // Setup
+        Product filterMock = new Product("prod", "desc", null);
+        when(productService.getBody(any())).thenReturn(filterMock);
+
+        var categories = List.of(new CategoryDTO());
+        when(categoryService.list(any())).thenReturn(categories);
+
+        var products = List.of(
+                ProductMapper.base(new Product("prod1", "desc1", BigDecimal.valueOf(50))),
+                ProductMapper.base(new Product("prod2", "desc2", BigDecimal.valueOf(50)))
+        );
+
+        var pageableMock = PageResponse.<ProductDTO>builder()
+                .content(products)
+                .currentPage(1)
+                .pageSize(2)
+                .sort(Sort.by("id").ascending())
+                .build();
+
+        when(productService.getAllPageable(any(), any(Mapper.class))).thenReturn(pageableMock);
+        when(productService.calculateTotalPriceFor(any())).thenReturn(BigDecimal.valueOf(100));
+
+        // Execution
+        IServletResponse response = productController.list(request);
+
+        // Verification
+        assertNotNull(response);
+        assertEquals(200, response.statusCode());
+
+        // Verify pageable content
+        var pageable = response.body().stream()
+                .filter(pair -> "pageable".equals(pair.key()))
+                .findFirst()
+                .map(e -> (IPageable<ProductDTO>) e.value())
+                .orElseThrow(() -> new AssertionError("Pageable not found"));
+
+        long counter = pageable.getContent().size();
+        assertEquals(2, counter);
+
+        // Verify total price
+        BigDecimal totalPrice = response.body().stream()
+                .filter(pair -> "totalPrice".equals(pair.key()))
+                .findFirst()
+                .map(e -> (BigDecimal) e.value())
+                .orElse(null);
+
+        assertEquals(BigDecimal.valueOf(100), totalPrice);
+
+        // Verify categories
+        var responseCategories = response.body().stream()
+                .filter(pair -> "categories".equals(pair.key()))
+                .findFirst()
+                .map(e -> (Collection<CategoryDTO>) e.value())
+                .orElse(null);
+
+        assertEquals(categories, responseCategories);
+
+        // Verify interactions
+        verify(productService, times(1)).getBody(request);
+        verify(productService, times(1)).getAllPageable(any(), any(Mapper.class));
+        verify(productService, times(1)).calculateTotalPriceFor(filterMock);
+        verify(categoryService, times(1)).list(any());
+    }
+
+    @Test
+    @DisplayName(
+            "Test delete method to remove a product. " +
+            "It should return a 200 status code and the expected response.")
+    void testDeleteProduct() throws ServiceException {
+        doReturn(true).when(productService).delete(request);
+
+        IHttpResponse<Void> response = productController.delete(request);
+
+        assertNotNull(response);
+        assertEquals(200, response.statusCode());
+        verify(productService, times(1)).delete(request);
+    }
+
+    @Test
+    @DisplayName(
+            "Test listById method to retrieve a product by ID. " +
+            "It should return a 200 status code and the expected response.")
+    void testGetById() throws ServiceException {
+        ProductDTO productDTO = new ProductDTO();
+        productDTO.setId(UUID.randomUUID().toString());
+
+        when(productService.findById(request)).thenReturn(productDTO);
+
+        IHttpResponse<ProductDTO> response = productController.getById(request);
+        assertNotNull(response);
+        assertEquals(200, response.statusCode());
+        assertEquals(productDTO, response.body());
+        verify(productService, times(1)).findById(request);
+    }
+}